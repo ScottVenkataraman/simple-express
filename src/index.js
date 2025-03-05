85ac71c0ddce1ead293861ef173417e9f5028d77
Scott Venkataraman <scottvenkataraman22@gmail.com>
2025-03-05 15:49:09 -0500

    Small changes, added port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+require('dotenv').config(); // Load environment variables
 // import 'dotenv/config';
+const express = require('express');
+
+
+const app = express();
+const port = 3000;
 
 console.log('hello there');
 console.log('general Kenobi');
